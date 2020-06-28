a655556fc4e4cc382390e8733d7d897a0f559821
Hyd3 <63372753+0Hyd3@users.noreply.github.com>
2020-06-28 20:09:40 -0300

    New Version 1.8

--- lizard.py
+++ lizard.py
@@ -4,7 +4,11 @@ import time
 import socket
 import random
 import requests
-
+#"╔══════════╗"
+#"║  ║"
+#"╚══════════╝"
+# ═╣ ╠═
+maxi = 10
 memo1 = "0"
 memo2 = "1"
 memo3 = "2"
@@ -39,16 +43,6 @@ for x in range(9000):
   print(chr(27)+'[2j')
   print('\033c')
   print('\x1bc')
- if res == "explorer":
-  
-  print("\033[91m[+]\033[97m Starting...")
-  
-  time.sleep(.5)
-  print(chr(27)+'[2j')
-  print('\033c')
-  print('\x1bc')
-  print("\033[92m#\033[97m Use CTRL+C to exit")
-  os.system("python3 explorer.py")
  if res == "reboot":
   print(chr(27)+'[2j')
   print('\033c')
@@ -59,32 +53,44 @@ for x in range(9000):
  if res == "sys":
   
 
-  print('\033[97m Created By HydeX')
-  print('\033[97m LizardOS Baseado em Python 3.7')
-  print('\033[91m 1.6 \033[97m | \033[92mAlligator Version')
+  print('\033[97m Created By Marcus V')
+  print('\033[97m LizardOS Modular Baseado em Python 3.7')
+  print('\033[91m 1.8 \033[97m | \033[92mRattlesnake Version')
  if res == "modules":
-  print("------------------\033[92mDEVELOPER ZONE\033[97m------------------")
-  print("|                                                |")
-  print('|                  BUILD, RUN                    |')
-  print("|                                                |")
-  print("--------------------------------------------------")
+	 # ═╣ ╠═
+  print("╔═════════════════ \033[92mDEVELOPER ZONE \033[97m═════════════════╗")
+  print("║                                                  ║")
+  print("╠══════════════════════════════════════════════════╣")
+  print("║                                                  ║")
+  print('║             Build , Run , Import[OFF]            ║')
+  print("║                                                  ║")
+  print("╚══════════════════════════════════════════════════╝")
 
   escl = input('\033[97mDev\033[92m>  	\033[97m')
   if escl == "run":
-   print('\033[92m#  \033[97mDon`t need ".py" on final of file')
+   print(chr(27)+'[2j')
+   print('\033c')
+   print('\x1bc')
+   print('\033[92m#  \033[97mDon`t need ".mod" on final of file')
    pop1 = input("\033[97mModule Name\033[92m> \033[97m")
+   os.rename(str(pop1) + ".module",str(pop1) + ".py")
    os.system("python3 " + str(pop1) + ".py")
+   os.rename(str(pop1) + ".py",str(pop1) + ".module")
   if escl == "build":   
+   
    print('\033[92m#  \033[97mUse this command to create a module to lizardOS ')
+   print(chr(27)+'[2j')
+   print('\033c')
+   print('\x1bc')
    mod1 = input("\033[97mModule Name\033[92m>      \033[97m")
-   os.mknod(mod1 + ".py")
-  
-   my_file = open(mod1 + ".py", "w")
-   text_list = ["# This is a module for LizardOS\n", "print('Hello World! i am " +mod1+ ".py a module to lizardOS')"] 
+   os.mknod(mod1 + ".module")
+
+   my_file = open(mod1 + ".module", "w")
+   text_list = ["# This is a module for LizardOS\n", "print('Hello World! i am " +mod1+ " a module to lizardOS')"] 
 
    my_file.writelines(text_list) 
 
-   my_file = open(mod1 + ".py")
+   my_file = open(mod1 + ".module")
 
 
    content = my_file.read()
@@ -94,21 +100,28 @@ for x in range(9000):
     
    print('\033[92m#  \033[97mThe module: ' + mod1 + ' is created!')
    print('\033[91m#  \033[97m Use "run" to view your module')
+
  if res == "echo":
   res = input("\033[97m ")
+  
+  
+ if res == "modrun":
+  print(chr(27)+'[2j')
+  print('\033c')
+  print('\x1bc')
+  print('\033[92m#  \033[97mDon`t need ".module" on final of file')
+  print('\033[91m#  \033[97mAlert you are using a fast run!')
+  pop1 = input("\033[97mModule Name\033[92m> \033[97m")
+  os.rename(str(pop1) + ".module",str(pop1) + ".py")
+  os.system("python3 " + str(pop1) + ".py")
+  os.rename(str(pop1) + ".py",str(pop1) + ".module")
 
  if res == "ping":
   pop1 = input("\033[97mIP\033[92m> \033[97m")
   print('\033[92m#  \033[97mUse CTRL+C to stop! ')
 
   os.system("ping " + str(pop1) + " -c 5")
- if res == "run":
-  print('\033[92m#  \033[97mUse this command to run python3 script or lizardmodules ')
-  pop1 = input("\033[97mScript\033[92m> \033[97m")
-  print('\033[92m#  \033[97mUse CTRL+C to stop! ')
 
-  os.system("python3 " + str(pop1))
-  
  if res == "dir":
 	 
   os.system("dir")
@@ -117,42 +130,60 @@ for x in range(9000):
   os.system(pop1)
  if res == "date":
   os.system("date")
- if res == "netconfig":
-  os.system("ifconfig")
- if res == "set":
-  print("----------------------\033[92mMEMORYS\033[97m---------------------")
-  print("|                                                |")
-  print('|        memo1, memo2, memo3, cursor(STRING)     |')
-  print("|                                                |")
-  print("--------------------------------------------------")
+ if res == "memorys":
+	 # ═╣ ╠═
+  print("╔═════════════════ \033[92mMemory  ZONE \033[97m═══════════════════╗")
+  print("║                                                  ║")
+  print("╠══════════════════════════════════════════════════╣")
+  print("║                                                  ║")
+  print('║             Edit , View , Import[OFF]            ║')
+  print("║                                                  ║")
+  print("╚══════════════════════════════════════════════════╝")
 
-  escl = input('\033[97mMemory\033[92m>  	\033[97m')
-  if escl == "memo1":   
-   print('\033[97m[Memory One]\033[97m \033[91m 2,5 Kb \033[97m/\033[92m 10 Kb')
-   memo1 = input('\033[97mSet\033[92m>  	\033[97m') 
-  if escl == "memo2":   
-   print('\033[97m[Memory Two]\033[97m \033[91m 2,9 Kb \033[97m/\033[92m 10 Kb')
-   memo2 = input('\033[97mSet\033[92m>  	\033[97m') 
-  if escl == "memo3":   
-   print('\033[97m[Memory Three]\033[97m \033[91m 3,1 Kb \033[97m/\033[92m 10 Kb')
-   memo3 = input('\033[97mSet\033[92m>  	\033[97m') 
-  if escl == "cursor":   
-   print('\033[97m[ATUAL: '+curso+' ]\033[97m')
-   curso = input('') 
- if res == "view":
-  print("----------------------\033[92mMEMORYS\033[97m---------------------")
-  print("|                                                |")
-  print('|        memo1, memo2, memo3, cursor(STRING)     |')
-  print("|                                                |")
-  print("--------------------------------------------------")
-  escl = input('\033[97mView\033[92m>  	\033[97m')
-  if escl == "memo1":   
-   print(memo1)
-  if escl == "memo2":   
-   print(memo2)
-  if escl == "memo3":   
-   print(memo3)
-  	 
+  escl = input('\033[97m?\033[92m>  	\033[97m')
+  print(chr(27)+'[2j')
+  print('\033c')
+  print('\x1bc')
+  if escl == "edit":
+  
+   print("╔══════════════════════\033[92m EDIT \033[97m══════════════════════╗")
+   print("║                                                  ║")
+   print('║        memo1, memo2, memo3, cursor(STRING)       ║')
+   print("║                                                  ║")
+   print("╚══════════════════════════════════════════════════╝") 
+   
+   escl = input('\033[97mMemory\033[92m>  	\033[97m')
+   if escl == "memo1":   
+    print('\033[97m[\033[92mSelected Memory One\033[97m]')
+    memo1 = input('\033[97mSet\033[92m>  	\033[97m') 
+   if escl == "memo2":   
+    print('\033[97m[\033[92mSelected Memory Two\033[97m]')
+    memo2 = input('\033[97mSet\033[92m>  	\033[97m') 
+   if escl == "memo3":   
+    print('\033[97m[\033[92mSelected Memory Three\033[97m]')
+    memo3 = input('\033[97mSet\033[92m>  	\033[97m') 
+   if escl == "cursor":   
+    print('\033[97m[\033[92mSelected Cursor(STRING)\033[97m]')
+    curso = input('') 
+   print(chr(27)+'[2j')
+   print('\033c')
+   print('\x1bc')
+  if escl == "view":
+   print("╔══════════════════════\033[92m VIEW \033[97m══════════════════════╗")
+   print("║                                                  ║")
+   print('║        memo1, memo2, memo3, cursor(STRING)       ║')
+   print("║                                                  ║")
+   print("╚══════════════════════════════════════════════════╝")
+   escl = input('\033[97mView\033[92m>  	\033[97m')
+   if escl == "memo1":   
+    print(memo1)
+   if escl == "memo2":   
+    print(memo2)
+   if escl == "memo3":   
+    print(memo3)
+   print(chr(27)+'[2j')
+   print('\033c')
+   print('\x1bc')	  
   
  if res == "update":
     import requests
@@ -173,8 +204,9 @@ for x in range(9000):
 
 
  if res == "get":
-  url = input('\033[97m Download-URL: \033[97m')
-  fileName = input('\033[97m FileName: \033[97m')
+  url = input('\033[97mDownload-URL\033[92m> \033[97m')
+  print("\033[91m#\033[97m Ex: 'file.extension'")
+  fileName = input('\033[97mFile-Name\033[92m> \033[97m')
   print("\033[91m[+]\033[97m Baixando...")
 
   req = requests.get(url)
